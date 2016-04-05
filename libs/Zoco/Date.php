42cd5604c1c3e6c82b86d6ea28aa0c9475b4c915
yezuozuo <1062276629@qq.com>
2016-04-05 10:42:27 +0800

    zoco add some date functions

--- libs/Zoco/Date.php
+++ libs/Zoco/Date.php
@@ -70,4 +70,58 @@ class Date {
         }
         return $wc + ceil($md / 7);
     }
+
+    /**
+     * 给定一个日期，获取其本月的第一天
+     * @param $date
+     * @return bool|string
+     */
+    static public function getCurMonthFirstDay($date) {
+        return date('Y-m-01', strtotime($date));
+    }
+
+    /**
+     * 给定一个日期，获取其本月的最后一天
+     * @param $date
+     * @return bool|string
+     */
+    static public function getCurMonthLastDay($date) {
+        return date('Y-m-d', strtotime(date('Y-m-01', strtotime($date)) . ' +1 month -1 day'));
+    }
+
+    /**
+     * 给定一个日期，获取其下月的第一天
+     * @param $date
+     * @return bool|string
+     */
+    static public function getNextMonthFirstDay($date) {
+        return date('Y-m-d', strtotime(date('Y-m-01', strtotime($date)) . ' +1 month'));
+    }
+
+    /**
+     * 给定一个日期，获取其下月的最后一天
+     * @param $date
+     * @return bool|string
+     */
+    static public function getNextMonthLastDay($date) {
+        return date('Y-m-d', strtotime(date('Y-m-01', strtotime($date)) . ' +2 month -1 day'));
+    }
+
+    /**
+     * 给定一个日期，获取其上月的第一天
+     * @param $date
+     * @return bool|string
+     */
+    static public function getPrevMonthFirstDay($date) {
+        return date('Y-m-d', strtotime(date('Y-m-01', strtotime($date)) . ' -1 month'));
+    }
+
+    /**
+     * 给定一个日期，获取其上月的最后一天
+     * @param $date
+     * @return bool|string
+     */
+    static public function getPrevMonthLastDay($date) {
+        return date('Y-m-d', strtotime(date('Y-m-01', strtotime($date)) . ' -1 day'));
+    }
 }
